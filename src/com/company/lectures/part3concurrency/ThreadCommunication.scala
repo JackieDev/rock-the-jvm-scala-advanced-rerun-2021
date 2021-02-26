0cf6c17c26943118b49814c041701b9393523fb8
JackieDev <jacquelinehubbard88@gmail.com>
2021-02-26 18:27:37 +0000

    Completed lecture 24: Producer-Consumer, Level 2

--- src/com/company/lectures/part3concurrency/ThreadCommunication.scala
+++ src/com/company/lectures/part3concurrency/ThreadCommunication.scala
@@ -1,5 +1,8 @@
 package com.company.lectures.part3concurrency
 
+import scala.collection.mutable
+import scala.util.Random
+
 object ThreadCommunication extends App {
 
   /*
@@ -76,5 +79,68 @@ object ThreadCommunication extends App {
     producer.start()
   }
 
-  smartProdCons()
+  //smartProdCons()
+
+
+  /*
+    producer -> [ ? ? ? ] -> consumer
+
+   */
+
+  def prodConsLargeBuffer(): Unit = {
+    val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
+    val capacity = 3
+
+    val consumer = new Thread(() => {
+      val random = new Random()
+
+      while(true) {
+        buffer.synchronized {
+          if (buffer.isEmpty) {
+            println("[consumer] buffer empty, waiting ...")
+            buffer.wait()
+          }
+
+          // there must be at least ONE value in the buffer
+          val x = buffer.dequeue()
+          println("[consumer] consumed " + x)
+
+          // Hey producer, there's empty space available, come get some!!
+          buffer.notify()
+        }
+
+        Thread.sleep(random.nextInt(250))
+      }
+    })
+
+    val producer = new Thread(() => {
+      val random = new Random()
+      var i = 0
+
+      while(true) {
+        buffer.synchronized {
+          if(buffer.size == capacity) {
+            println("[producer] buffer is full, waiting...")
+            buffer.wait()
+          }
+
+          // there must be at least ONE EMPTY SPACE in the buffer
+          println("[producer] producing " + i)
+          buffer.enqueue(i)
+
+          // Hey consumer, new hot stuff for you!!!
+          buffer.notify()
+
+          i += 1
+        }
+
+        Thread.sleep(random.nextInt(250))
+      }
+    })
+
+    consumer.start()
+    producer.start()
+  }
+
+  prodConsLargeBuffer()
 }
