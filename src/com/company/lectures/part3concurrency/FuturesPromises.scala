a37491b8f787f56176bcaa7df0cec66225faac2f
JackieDev <jacquelinehubbard88@gmail.com>
2021-03-03 19:13:11 +0000

    Completed lectures 29 and 30: Futures Parts 3 and 4 + Exercises

--- src/com/company/lectures/part3concurrency/FuturesPromises.scala
+++ src/com/company/lectures/part3concurrency/FuturesPromises.scala
@@ -1,8 +1,9 @@
 package com.company.lectures.part3concurrency
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.util.{Failure, Random, Success}
+import scala.concurrent.{Await, Future, Promise}
+import scala.util.{Failure, Random, Success, Try}
+import scala.concurrent.duration._
 
 object FuturesPromises extends App {
 
@@ -98,5 +99,184 @@ object FuturesPromises extends App {
   }
 
   val fallbackResult = SocialNetwork.fetchProfile("unknown id").fallbackTo(SocialNetwork.fetchProfile("fb.id.0-dummy"))
+
+
+
+  // Lecture 29: Futures, Part 3
+  // online banking app
+  case class User(name: String)
+  case class Transaction(sender: String, receiver: String, amount: Double, status: String)
+
+  object BankingApp {
+    val name = "Rock the JVM banking"
+
+    def fetchUser(name: String): Future[User] = Future {
+      // simulate fetching from a DB
+      Thread.sleep(500)
+      User(name)
+    }
+
+    def createTransaction(user: User, merchantName: String, amount: Double): Future[Transaction] = Future {
+      // simulate some processes
+      Thread.sleep(1000)
+      Transaction(user.name, merchantName, amount, "SUCCESS")
+    }
+
+    def purchase(username: String, item: String, merchantName: String, cost: Double): String = {
+      // fetch the user from the DB
+      // create a transaction
+      // wait for the transaction to finish
+
+      val transactionStatusFuture = for {
+        user <- fetchUser(username)
+        transaction <- createTransaction(user, merchantName, cost)
+      } yield transaction.status
+
+      Await.result(transactionStatusFuture, 2.seconds)
+    }
+  }
+
+  println(BankingApp.purchase("Daniel", "cheap iPhone 12", "rock the jvm store", 99))
+
+
+  // promises
+  val promise = Promise[Int]() // a controller over a Future
+  val future = promise.future
+
+  // thread 1 - "consumer"
+  future.onComplete {
+    case Success(r) => println("[consumer] I've received " + r)
+  }
+
+  // thread 2 - "producer"
+  val producer = new Thread(() => {
+    println("[producer] crunching numbers ...")
+    Thread.sleep(500)
+    // "fulfilling" the promise
+    promise.success(42)
+    println("[producer] done")
+  })
+
+  producer.start()
+  Thread.sleep(1000)
+
+
+  // Lecture 30
+  /*
+     Exercises
+     1) fulfill a future IMMEDIATELY with a value
+     2) inSequence(fa, fb)
+     3) first(fa, fb) => new future with the first value of the two futures
+     4) last(fa, fb) => new future with the last value
+     5) retryUntil(action: () => Future[T], condition: T => Boolean): Future[T]
+   */
+
+  // My attempts at these exercises
+  // 1) fulfill
+  val forfilledFuture = Future[Int](10)
+
+  // 2) inSequence
+  // hmmmm
+
+  // 3) first
+  def first[T](list: List[T]): Future[T] = Future(list.head)
+
+  // 4) last
+  def last[T](list: List[T]): Future[T] = Future(list.reverse.head)
+
+  // 5) retryUntil
+  //def retryUntil[T](action: () => Future[T], condition: T => Boolean): Future[T] = Future {
+//    def go(newCondition: Boolean): Future[T] = {
+//      if (newCondition) action
+//      else go(condition.apply())
+//    }
+
+    //go(action(condition))
+
+//    if (condition)
+//    action
+//    else ???
+//  }
+
+
+  // Daniel's solutions to these exercises
+  // 1) fulfill
+  def fulfillImmediately[T](value: T): Future[T] = Future(value) // this is similar to my solution, I should have used a def
+
+  // 2) inSequence
+  def inSequence[A, B](first: Future[A], second: Future[B]): Future[B] =
+    first.flatMap(_ => second)
+
+  // 3) first one to complete to be returned - I got the wrong end of the stick above haha
+  def first[A](fa: Future[A], fb: Future[A]): Future[A] = {
+    val promise = Promise[A]
+
+    def tryComplete(promise: Promise[A], result: Try[A]) = result match {
+      case Success(r) => try {
+        promise.success(r)
+      } catch {
+        case _ =>
+      }
+      case Failure(t) => try {
+        promise.failure(t)
+      } catch {
+        case _ =>
+      }
+    }
+
+    fa.onComplete(tryComplete(promise, _))
+    fb.onComplete(tryComplete(promise, _))
+
+    promise.future
+  }
+
+  // 4) last one to complete to be returned
+  def last[A](fa: Future[A], fb: Future[A]): Future[A] = {
+    val bothPromise = Promise[A]
+    val lastPromise = Promise[A]
+    val checkAndComplete = (result: Try[A]) =>
+    if(!bothPromise.tryComplete(result))
+        lastPromise.complete(result)
+
+    fa.onComplete(checkAndComplete)
+    fb.onComplete(checkAndComplete)
+
+    lastPromise.future
+  }
+
+  val fast = Future {
+    Thread.sleep(100)
+    42
+  }
+
+  val slow = Future {
+    Thread.sleep(200)
+    45
+  }
+
+  first(fast, slow).foreach(println)
+  last(fast, slow).foreach(println)
+
+  Thread.sleep(1000)
+
+
+  // 5) retryUntil
+  def retryUntil[A](action: () => Future[A], condition: A => Boolean): Future[A] =
+    action()
+    .filter(condition)
+    .recoverWith {
+      case _ => retryUntil(action, condition)
+    }
+
+  val random = new Random()
+  val action = () => Future {
+    Thread.sleep(100)
+    val nextValue = random.nextInt(100)
+    println("generated " + nextValue)
+    nextValue
+  }
+
+  retryUntil(action, (x: Int) => x < 10).foreach(result => println("settled at " + result))
+  Thread.sleep(10000)
 
 }
