30f57034c01fa007cbb3a37de82712b0581ad123
JackieDev <jacquelinehubbard88@gmail.com>
2021-03-24 18:55:54 +0000

    Completed lectures 53 and 54, so have completed THE COURSE

--- src/com/company/lectures/part5ts/Reflection.scala
+++ src/com/company/lectures/part5ts/Reflection.scala
@@ -43,4 +43,59 @@ object Reflection extends App {
   // 5 - invoke the method
   method.apply()
 
+
+  // The last lecture
+  // type erasure
+
+  // pain point #1: cannot differentiate types at runtime
+  val numbers = List(1,2,3)
+  numbers match {
+    case listOfStrings: List[String] => println("list of strings")
+    case listOfNumbers: List[Int] => println("list of numbers")
+  }
+
+  // pain point #2: limitations on overloads as list types will both get reduced to just List so they are identical
+  //def processList(list: List[Int]): Int = 42
+  //def processList(list: List[String]): Int = 45
+
+  // we can solve this problem ^
+  // Enter: TypeTags
+
+  // 0 - import
+  import ru._
+
+  // 1 - creating a tag type manually
+  val ttag = typeTag[Person]
+  println(ttag.tpe)
+
+  class MyMap[K, V]
+
+  // 2 - pass type tags as implicit parameters
+  def getTypeArguments[T](value: T)(implicit typeTag: TypeTag[T]) = typeTag.tpe match {
+    case TypeRef(_, _, typeArguments) => typeArguments
+    case _ => List()
+  }
+
+  val myMap = new MyMap[Int, String]
+  val typeArgs = getTypeArguments(myMap)//(typeTag: TypeTag[MyMap[Int,String]])
+  println(typeArgs)
+
+  def isSubtype[A, B](implicit ttagA: TypeTag[A], ttagB: TypeTag[B]): Boolean = {
+    ttagA.tpe <:< ttagB.tpe
+  }
+
+  class Animal
+  class Dog extends Animal
+  println(isSubtype[Dog, Animal])
+
+
+  // I have an instance
+
+  // 3 - method symbol
+  val anotherMethodSymbol = typeTag[Person].tpe.decl(ru.TermName(methodName)).asMethod
+  // 4 - reflect the method = can DO things
+  val sameMethod = reflected.reflectMethod(anotherMethodSymbol)
+  // 5 - invoke the method
+  sameMethod.apply()
+
 }
